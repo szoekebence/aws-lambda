39aab5610c830cf762e8ae9c72d56aa3278da336
Szőke Bence <Bence.Szoke@idomsoft.hu>
2021-11-30 18:56:19 +0100

    fix return value in lambdas

--- src/main/resources/Function_Codes/JavaFibonacciFunctionHandler.java
+++ src/main/resources/Function_Codes/JavaFibonacciFunctionHandler.java
@@ -3,7 +3,7 @@ public class JavaFibonacciFunctionHandler {
     public float handleRequest() {
         long startTime = System.nanoTime();
         long f1 = 0L, f2 = 1L;
-        for (int i = 1; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             long next = f1 + f2;
             f1 = f2;
             f2 = next;
