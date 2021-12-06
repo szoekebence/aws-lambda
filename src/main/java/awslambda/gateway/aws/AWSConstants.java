873d37f587acb48ce4eda88e27aacc3bf13a5662
Szőke Bence <Bence.Szoke@idomsoft.hu>
2021-12-06 23:55:00 +0100

    remove sensitive data

--- src/main/java/awslambda/gateway/aws/AWSConstants.java
+++ src/main/java/awslambda/gateway/aws/AWSConstants.java
@@ -6,8 +6,8 @@ public final class AWSConstants {
 
     public static final String DB_HOST = "https://dynamodb.eu-central-1.amazonaws.com";
     public static final String REGION = "eu-central-1";
-    public static final String AWS_ACCESS_KEY_ID = "AKIA46D2LTA52FLZ5NGR";
-    public static final String AWS_SECRET_ACCESS_KEY = "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";
+    public static final String AWS_ACCESS_KEY_ID = null;
+    public static final String AWS_SECRET_ACCESS_KEY = null;
     public static final int NUMBER_OF_FUNCTION_CALLS_PER_CONFIGURATION = 100;
     public static final List<String> LAMBDA_FUNCTION_LANGUAGES = List.of("java", "python", "nodejs", "ruby");
     public static final List<String> LAMBDA_FUNCTION_ARCHITECTURES = List.of("x86_64", "arm64");
